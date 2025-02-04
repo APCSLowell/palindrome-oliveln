894d31561cde29ad6382d2d285de001feb605345
oliveln <143148891+oliveln@users.noreply.github.com>
2025-02-03 18:47:53 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -3,37 +3,38 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class PalindromeChecker {
-  public void tester()
-  {
-    //  String lines[] = loadStrings("palindromes.txt");
-    String[] lines = new String[6];
-    try {
-      File myFile = new File("palindromes.txt");
-      Scanner myReader = new Scanner(myFile);
-      int counter = 0;
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        lines[counter] = data;
-        counter++;
-      }
-      myReader.close();
+public void tester()
+{
+  //String lines[] = loadStrings("palindromes.txt");
+  String[] lines = new String[6]; 
+    try{
+        File myFile = new File("palindromes.txt");
+        Scanner myReader = new Scanner(myFile);
+        int counter = 0;
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            lines[counter] = data;
+            counter++;
+        }
+        myReader.close();
     }
     catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+        System.out.println("An error occurred.");
+        e.printStackTrace();
     }
-    System.out.println("there are " + lines.length + " lines");
-    for (int i=0; i < lines.length; i++)
+  System.out.println("there are " + lines.length + " lines");
+  for (int i=0; i < lines.length; i++) 
+  {
+    if(isPalindrome(lines[i])==true)
     {
-      if (isPalindrome(lines[i])==true)
-      {
-        System.out.println(lines[i] + " IS a palindrome.");
-      } else
-      {
-        System.out.println(lines[i] + " is NOT a palindrome.");
-      }
+      System.out.println(lines[i] + " IS a palindrome.");
+    }
+    else
+    {
+      System.out.println(lines[i] + " is NOT a palindrome.");
     }
   }
+}
   public boolean isPalindrome(String sWord) {
     String clean = noCapitals(sWord);
     clean = onlyLetters(clean);
@@ -76,4 +77,3 @@ public class PalindromeChecker {
     return empty;
   }
 }
-
